07b871cd2e608710966962a03735cee104f1eda1
Malhar1080 <malhar1080p@gmail.com>
2025-10-12 12:56:27 +0530

    refactor(styling):Made style changes to improve overall webpage design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import HeroCarousel from "./blocks/hero-carousel/hero-carousel";
+import ProductDisplay from "./blocks/product-display/product-display";
+import ContentCurator from "./blocks/content-curator/content-curator";
 
-export default function Home() {
+export default function HomePage() {
   return (
-    <main className="main">
-      <div className="main-content">
-      
-      </div>
-    </main>
+    <>
+      <HeroCarousel />
+      <ProductDisplay />
+      <ContentCurator />
+    </>
   );
 }
